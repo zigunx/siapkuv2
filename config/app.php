94e7e8ccea1f12902d3280bd9c87ff35de95793a
Hardi Agunadi <hardiagunadi@gmail.com>
2016-03-17 11:50:18 +0700

    update

--- config/app.php
+++ config/app.php
@@ -155,6 +155,10 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+		
+		        // AdminLTE template provider
+        Acacha\AdminLTETemplateLaravel\Providers\AdminLTETemplateServiceProvider::class,
+
 
     ],
 
